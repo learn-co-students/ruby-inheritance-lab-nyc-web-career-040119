28736e10727ebadbed73298cf9f092bd0ccbe3e3
ferrisboran <ferris@ferryrules.com>
2019-04-07 21:15:56 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,12 +2,19 @@ class Student < User
 
   attr_accessor :knowledge
 
+  @@all = []
+
   def initialize
     @knowledge = []
+    @@all << self
   end
 
   def learn(string)
     @knowledge << string
+  end
+
+  def self.all
+    @@all
   end
 
 end
